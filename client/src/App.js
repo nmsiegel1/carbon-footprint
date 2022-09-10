d9d9bad519b2bc5bc9778240e4f2efce3cd7f039
Gilina McBride <gilinamcbride@gmail.com>
2022-09-10 18:19:54 -0400

    created react pages framework

---
+++ client/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import {
+//     ApolloProvider,
+//     ApolloClient,
+//     InMemoryCache,
+//     createHttpLink,
+//   } from "@apollo/client";
+// import { setContext } from "@apollo/client/link/context";
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Calculator from './pages/Calculator';
+import MyPledges from './pages/MyPledges';
+
+import Header from './components/Header';
+
+// const httpLink = createHttpLink({
+//     uri: "/graphql",
+//   });
+
+//   const authLink = setContext((_, { headers }) => {
+//     const token = localStorage.getItem("id_token");
+//     return {
+//       headers: {
+//         ...headers,
+//         authorization: token ? `Bearer ${token}` : "",
+//       },
+//     };
+//   });
+
+//   const client = new ApolloClient({
+//     link: authLink.concat(httpLink),
+//     cache: new InMemoryCache(),
+//   });
+
+function App() {
+  return (
+    // <ApolloProvider client={client}>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/mypledges" element={<MyPledges />} />
+        </Routes>
+      </div>
+    </Router>
+    // </ApolloProvider>
+  );
+}
+
+export default App;
