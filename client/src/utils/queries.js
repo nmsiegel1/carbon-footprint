804e2de9e87cd89645864649d069a0cdadc68fa2
Amanda Klenk <amandamklenk3@gmail.com>
2022-09-12 20:00:04 -0400

    set up queries in queries.js in utils directory of client/src

---
+++ client/src/utils/queries.js
@@ -0,0 +1,40 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      homeData {
+        _id
+        waterEmissions
+        electricityEmissions
+        heatEmissions
+      }
+      travelData {
+        _id
+        vehicleEmissions
+        publicTransitEmissions
+        planeEmissions
+      }
+      pledgeData {
+        _id
+        action
+        description
+        icon
+        link
+      }
+    }
+  }
+`;
+
+export const QUERY_PLEDGES = gql`
+  pledges {
+    _id
+    action
+    description
+    icon
+    link
+  }
+`;
