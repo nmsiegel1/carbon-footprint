1474fe2125a83ccc8db1f6d988a40a2333895de2
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-09-13 15:43:25 -0400

    added logo and updated some styles

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './style.css';
-import menu_icon from './menu.png';
+import menu_icon from './images/menu.png';
+import logo from './images/logo.png';
 
 const Navbar = () => {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
@@ -8,7 +9,11 @@ const Navbar = () => {
   return (
     <nav className="navBar">
       <a href="/" className="brand-name">
-        Carbon Footsteps
+        <img
+          src={logo}
+          alt="logo of foot outline with the earth inside of it"
+          className="navLogo"
+        />
       </a>
       <button
         className="hamburger"
@@ -16,16 +21,12 @@ const Navbar = () => {
           setIsNavExpanded(!isNavExpanded);
         }}
       >
-        <img
-          src={menu_icon}
-          alt="3 lines to representing the menu"
-          className="navLogo"
-        />
+        <img src={menu_icon} alt="3 lines to representing the menu" />
       </button>
       <div className={isNavExpanded ? 'menuNav expanded' : 'menuNav'}>
         <ul>
           <li>
-            <a href="/home">Home</a>
+            <a href="/">Home</a>
           </li>
           <li>
             <a href="/mypledges">Pledges</a>
