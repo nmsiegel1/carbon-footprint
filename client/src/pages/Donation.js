2bb3e69341327db3b9db01cfda425f9a129b8f47
Gilina McBride <gilinamcbride@gmail.com>
2022-09-14 14:25:27 -0400

    added donation.js and updated app.js

---
+++ client/src/pages/Donation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Donation = () => {
+  return (
+    <div className="donation">
+      <p>Other ways to help.</p>
+    </div>
+  );
+};
+
+export default Donation;
