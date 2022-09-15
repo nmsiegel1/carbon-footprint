820e5027b44a3850df8dbdac9d80c4fe6ea3d77d
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-09-14 20:40:35 -0400

    add single pledge component

--- client/src/components/Pledges/index.js
+++ client/src/components/Pledges/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_PLEDGES } from '../../utils/queries';
 
 const Pledges = () => {
-  return <div>These are pledges to choose from</div>;
+  const data = useQuery(QUERY_PLEDGES);
+
+  const pledges = data.pledges || [];
+
+  return (
+    <div>
+      These are pledges to choose from
+      <h2>Pledge to reduce your carbon footprint!</h2>
+      {pledges.length(<div>{pledges.action}</div>)}
+    </div>
+  );
 };
 
 export default Pledges;
