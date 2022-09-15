6f8024686f54a40e6e90180f6deacb46cebae4cd
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-09-15 11:37:55 -0400

    my pledges rendering on page

--- client/src/pages/MyPledges.js
+++ client/src/pages/MyPledges.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Icon } from '@iconify/react';
+import { useQuery, useMutation } from '@apollo/client';
+
+import { QUERY_PLEDGES, QUERY_ME } from '../utils/queries';
+import { REMOVE_PLEDGE } from '../utils/mutations';
 
 const MyPledges = () => {
-  return <div>My Pledges</div>;
+  const data = useQuery(QUERY_ME);
+  console.log('data', data);
+  const myPledges = data.data.me?.pledgeData || [];
+  console.log('my pledges', myPledges);
+  // const myPledges = data.
+  return (
+    <div>
+      <h2>My Pledges</h2>
+      <div>
+        {myPledges.map((pledge) => (
+          <div key={pledge.action}>
+            <div>
+              <h3>
+                <Icon
+                  icon={pledge.icon}
+                  color="#243B4A"
+                  width="20"
+                  height="20"
+                />
+                {pledge.action}
+              </h3>
+              <p>{pledge.description}</p>
+              <a href={pledge.link} target="_blank" rel="noopener noreferrer">
+                Learn more about this action
+              </a>
+              <button>Remove Pledge</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyPledges;
