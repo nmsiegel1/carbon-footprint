cdbe92f51efaf9bec1fe47b5d61f91daf4703c54
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-09-18 15:57:10 -0400

    stylised footer to work on all pages

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,9 +4,15 @@ import './style.css';
 function Footer(props) {
   return (
     <footer className="footer">
-      <div className="row">
-        <div className="col-md-12 mt-2 mb-0">
-          <p className="mb-0">
+      <div className="contain">
+        <div className="col">
+          <h1>Carbon Footsteps</h1>
+          <ul>
+            <li>About</li>
+            <li>Calculator</li>
+            <li>Contact Us</li>
+          </ul>
+          {/* <p className="mb-0">
             <a
               href="https://github.com/nmsiegel1/carbon-footprint"
               rel="noreferrer"
@@ -20,18 +26,40 @@ function Footer(props) {
             <a href="/mypledges">
               <i className="fa fa-check fa-lg" aria-hidden="true"></i>
             </a>
-          </p>
+          </p> */}
         </div>
       </div>
-
-      <div className="row mb-3">
-        <div className="col-md-12 copyright">
-          <p>
-            <i className="fa fa-copyright" aria-hidden="true"></i> CARBON
-            FOOTSTEPS
-          </p>
-        </div>
+      <div className="col">
+        <h1>Learn More</h1>
+        <ul>
+          <li>
+            <a
+              href="https://github.com/nmsiegel1/carbon-footprint"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <i className="fa fa-github fa-md" aria-hidden="true"></i>
+              Github Repository
+            </a>
+          </li>
+          <li>
+            <a href="/donation">
+              <i className="fa fa-wallet fa-md" aria-hidden="true"></i>
+              Donate
+            </a>
+          </li>
+          <li>
+            <a href="/mypledges">
+              <i className="fa fa-check fa-md" aria-hidden="true"></i>
+              My pledges
+            </a>
+          </li>
+        </ul>
       </div>
+      <div className=" col copyright">
+        <i className="fa fa-copyright" aria-hidden="true"></i> 2022 Copyright
+      </div>
+      <div className="clearfix"></div>
     </footer>
   );
 }
