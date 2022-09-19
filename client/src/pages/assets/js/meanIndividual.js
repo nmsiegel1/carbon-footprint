4901867032cbd996fe9f2c7d30215ca999b21b41
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-09-19 07:18:04 -0400

    added average individual emissions to homepage and merged develop to home branch

---
+++ client/src/pages/assets/js/meanIndividual.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: [
+    'Electricity Emissions',
+    'Vehicle Emissions',
+    'Plane Emissions',
+    'Water Emissions',
+    'Heat Emissions',
+    'Public Transit Emissions',
+  ],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [13120, 7405, 3000, 13120, 6690, 500],
+      backgroundColor: [
+        'rgba(162, 213, 159, 1)',
+        'rgba(98, 187, 160, 1)',
+        'rgba(72, 139, 118, 1)',
+        'rgba(27, 80, 109, 1)',
+        'rgba(44, 130, 179, 1)',
+        'rgba(155, 209, 229, 1)',
+      ],
+      borderColor: [
+        'rgba(36, 59, 74, 1)',
+        'rgba(36, 59, 74, 1)',
+        'rgba(36, 59, 74, 1)',
+        'rgba(36, 59, 74, 1)',
+        'rgba(36, 59, 74, 1)',
+        'rgba(36, 59, 74, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const MeanIndividual = () => {
+  return (
+    <div>
+      <Doughnut
+        data={data}
+        height={'400px'}
+        width={'400px'}
+        options={{ maintainAspectRatio: false, resizeDelay: 0 }}
+      ></Doughnut>
+    </div>
+  );
+};
+
+export default MeanIndividual;
